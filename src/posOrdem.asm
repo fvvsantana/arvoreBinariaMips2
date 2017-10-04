8407988804e3378ba1304602b0da0fcead07da4e
Unknown <leonardo.feltran@gmail.com>
2017-10-04 13:48:52 -0300

    Altera registradores usados e corrige comentarios

--- src/posOrdem.asm
+++ src/posOrdem.asm
@@ -3,47 +3,47 @@
 .text
 	.globl posOrdem
 	posOrdem:	
-		#Coloca no resistrador s0 o endereço da raiz da arvore
-		lw $s0, 0($a0)
+		#Coloca no resistrador s1 o endereco da raiz da arvore
+		lw $s1, 0($a0)
 		
-		#Coloca em a0 o endereço da raiz da arvore
-		move $a1, $s0
+		#Coloca em a1 o endereco da raiz da arvore
+		move $a1, $s1
 		
 		pos:
 			#Desloca o stack pointer
 			sub $sp, $sp, 4
 		
-			#Armazena o endereço de $fp atual na stack
+			#Armazena o endereco de fp atual na stack
 			sw $fp, 0($sp)
 		
-			#Atualiza o $fp para o local aonde está armazenado o fp da chamada anterior
+			#Atualiza fp para o local aonde esta armazenado o fp da chamada anterior
 			move $fp, $sp
 		
 			#Desloca o stack pointer em duas posicoes
 			sub $sp, $sp, 8
 		
-			#Salva o endereco da raiz no registrador s0
-			move $s0, $a1
+			#Salva o endereco da raiz no registrador s1
+			move $s1, $a1
 		
 			#Armazena os enderecos de retorno e da arvore na stack
 			sw $ra, -4($fp)
-			sw $s0, -8($fp)
+			sw $s1, -8($fp)
 						
-			#Coloca em a0 o endereco do no esquerdo
-			lw $a1, 4($s0)
+			#Coloca em a1 o endereco do no esquerdo
+			lw $a1, 4($s1)
 			
-			#Verifica se há nó esquesdo. Caso negativo, procura nó direito
+			#Verifica se ha no esquerdo. Caso negativo, procura no direito
 			beq $a1, -1, direita
 			
-			#Caro positivo, procura por filhos do nó esquerdo
+			#Caso positivo, procura por filhos do no esquerdo
 			jal pos
 		
 		direita:
 			
-			#Carraga em s0 o endereco do elementa da direita
-			lw $a1, 8($s0)
+			#Carraga em s1 o endereco do elemento da direita
+			lw $a1, 8($s1)
 			
-			#Se nao existir nó direito, printa e volta à execucao anterior
+			#Se nao existir no direito, printa e volta a execucao anterior
 			beq $a1, -1, retorna
 			
 			#Caso exista no direito, faz a procura de seus filhos
@@ -53,7 +53,7 @@
 			
 			
 			#Imprime o elemento da arvore
-			lw $a0, 0($s0)
+			lw $a0, 0($s1)
 			li $v0, 1
 			syscall
 			
@@ -65,19 +65,18 @@
 			#Faz o stack pointer apontar para o inicio da pilha da execucao atual
 			move $sp, $fp
 			
-			#Coloca em ra o endereço aonde a execucao anterior parou
+			#Coloca em ra o endereco aonde a execucao anterior parou
 			lw $ra, -4($fp)
 			
 			#Atualiza fp para o inicio da stack da execucao anterior
 			lw $fp, 0($sp)
 			
-			#Carrega em s0 o endereço da raiz da raiz da execução anterior
-			lw $s0, -8($fp)
+			#Carrega em s1 o endereco da raiz da execucao anterior
+			lw $s1, -8($fp)
 			
 			#Desloca o stack pointer para o ultimo elemento da pilha
 			add $sp, $sp, 4
 			
 			#Retorna para o ponto de onde parou na execucao anterior
 			jr $ra
-							
-		
+			
