b208f0953441df4806ce73ec842de46ae267b1a1
Unknown <leonardo.feltran@gmail.com>
2017-10-05 15:17:56 -0300

    Retira a dependência do resgistrador $fp

--- src/posOrdem.asm
+++ src/posOrdem.asm
@@ -5,17 +5,11 @@
 	
 		posOrdem:			
 			#Aloca memoria para receber dados da execucao atual
-			sub $sp, $sp, 12
+			sub $sp, $sp, 8
 		
-			#Armazena o endereco de fp atual na stack
-			sw $fp, 8($sp)
-		
-			#Atualiza fp para o local aonde esta armazenado o fp da chamada anterior
-			add $fp, $sp, 8
-		
-			#Armazena os enderecos de retorno e do no na stack
-			sw $ra, -4($fp)
-			sw $a0, -8($fp)
+			#Armazena o endereco de retorno e do no na stack
+			sw $a0, 0($sp)
+			sw $ra, 4($sp)
 			
 			#Verifica se o endereco analizado atualmente e invalido
 			#Caso afirmativo, termina a execução
@@ -26,14 +20,14 @@
 			jal posOrdem
 			
 			#Carrega em a0 o endereco da raiz da execucao atual
-			lw $a0, -8($fp)
+			lw $a0, 0($sp)
 			
 			#Coloca em a0 o endereço do no direito e executa o percorrimento
 			lw $a0, 8($a0)
 			jal posOrdem
 			
 			#Carrega em a0 o endereco da raiz da execucao atual
-			lw $a0, -8($fp)
+			lw $a0, 0($sp)
 			
 			#Imprime o elemento da arvore
 			lw $a0, 0($a0)
@@ -47,10 +41,7 @@
 			
 		return:
 			#Coloca em ra o endereco de aonde a execucao anterior parou
-			lw $ra, -4($fp)
-			
-			#Atualiza o valor de fp para o valor da execução anterior
-			lw $fp, 0($fp)
+			lw $ra, 4($sp)
 			
 			#Desaloca memória da execução anterior
 			add $sp, $sp, 8
