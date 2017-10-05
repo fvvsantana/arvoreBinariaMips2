b54f836d367206624f21e4bd53272f4f01de3861
Unknown <cesaralbc@gmail.com>
2017-10-05 15:30:45 -0300

    Implementa a função emOrdem

--- src/emOrdem.asm
+++ src/emOrdem.asm
@@ -1,8 +1,46 @@
 .data
+	newline: .asciiz "\n"
 
 .text
 	.globl emOrdem
-	emOrdem:
+	emOrdem:#(No* algum)
 		
-		jr $ra
+		# se o no atual estiver vazio, pular para o final
+		beq $a0, -1, finalizar
 	
+		# salva o endereco de retorno
+		sub $sp, $sp, 4
+		sw $ra, 0($sp)
+		
+		# salva o endereco desse no ($a0) antes de chamar a proxima funcao
+		sub $sp, $sp, 4
+		sw $a0, 0($sp)
+		
+		# chama a funcao para o no da esquerda
+		lw $a0, 4($a0)
+		jal emOrdem
+		
+		# recupera o endereco desse no
+		lw $t0, 0($sp)
+		add $sp, $sp, 4
+		
+		# imprime o valor desse no
+	 	lw $a0, 0($t0)
+	 	li $v0, 1
+	 	syscall
+	 	
+	 	# imprime newline
+		li $v0, 4
+		la $a0, newline
+		syscall
+	 	
+	 	# chama a funcao para o no da direita
+	 	lw $a0, 8($t0)
+	 	jal emOrdem
+		
+		# recupera o endereco de retorno
+		lw $ra, ($sp)
+		add $sp, $sp, 4
+		
+		finalizar:
+		jr $ra
