2539a4981f2e6d3cada71c6553426aa3a117d6df
Unknown <cesaralbc@gmail.com>
2017-10-09 21:12:09 -0300

    Implementa o menu

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,7 @@
 .data
-
+	opcoes:	.asciiz "\n\nDigite o numero correspondente a operacao desejada:\n 1- Insercao\n 2- Percorrimento pre-ordem\n 3- Percorrimento em-ordem\n 4- Percorrimento pos-ordem\n 5- Sair\n"
+	erro:	.asciiz "\nValor invalido, tente novamente."
+	valor:	.asciiz "Digite o valor:\n"
 .text
 	.globl main
 	main:
@@ -20,14 +22,70 @@
 		move $a0, $s0
 		jal criaArvore
 		
+		j menu
+		
 		#testa alocacao
-		move $a0, $s0
-		jal testInsere
-		
-		
+		#move $a0, $s0
+		#jal testInsere
 		
 		#volta $sp para o topo da stack
 		move $sp, $fp
+		
+	menu:
+		# imprime as opcoes
+		li $v0, 4
+		la $a0, opcoes
+		syscall
+		
+		# le o numero digitado
+		li $v0, 5
+		syscall
+		
+		# salva o valor em $s1
+		move $s1, $v0
+	
+		# define o arguemento padrao para a maioria das funcoes
+		lw $a0, ($sp)
+		
+		# verifica cada valor possivel
+		# digitar:
+		beq $s1, 1, digitaValor
+		
+		# em ordem:
+		beq $s1, 3, emOrdem 
+		
+		# sair:
+		beq $s1, 5, sair
+		
+		# se nenhum caso deu certo, foi valor invalido
+		li $v0, 4
+		la $a0, erro
+		syscall
+		
+		# volta para o comeco
+		j menu
+		
+	digitaValor:
+		# imprime string
+		li $v0, 4
+		la $a0, valor
+		syscall
+		
+		# obtem o valor
+		li $v0, 5
+		syscall
+		
+		# define os argumentos para insercao
+		move $a0, $v0
+		move $a1, $s0
+		
+		# chama a funcao de insercao
+		jal insere
+		
+		# volta para o menu
+		j menu
+		
+	sair:
 		#exit
 		li $v0, 10
 		syscall
